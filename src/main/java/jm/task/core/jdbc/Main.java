6a28678b9ce6e6a51b21cb0297bac3868733d859
Vladislav Shilov <Shilovvlad@mail.ru>
2022-11-29 00:08:49 +0300

    add Util for Hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,24 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
+
 import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        Util.getConnection();
-        UserService userServ = new UserServiceImpl();
+        Util.getSessionFactory();
+//        UserService userServ = new UserServiceImpl();
 
-        userServ.createUsersTable();
-
-        userServ.saveUser("Frodo", "Baggins", (byte) 50);
-        userServ.saveUser("Samwise", "Gamgee", (byte) 38);
-        userServ.saveUser("Merry", "Brandybuck", (byte) 36);
-        userServ.saveUser("Pippin", "Took", (byte) 28);
-
-        userServ.removeUserById(1);
-        userServ.getAllUsers();
-        userServ.cleanUsersTable();
-        userServ.dropUsersTable();
+//        userServ.createUsersTable();
+//
+//        userServ.saveUser("Frodo", "Baggins", (byte) 50);
+//        userServ.saveUser("Samwise", "Gamgee", (byte) 38);
+//        userServ.saveUser("Merry", "Brandybuck", (byte) 36);
+//        userServ.saveUser("Pippin", "Took", (byte) 28);
+//
+//        userServ.removeUserById(1);
+//        userServ.getAllUsers();
+//        userServ.cleanUsersTable();
+//        userServ.dropUsersTable();
     }
 }
